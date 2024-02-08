98de560499e07535c0ac5613252665ba09eba0e1
Adrián Darío Hidalgo Flores <adrian@bundly.dev>
2024-02-07 21:54:30 -0600

    Remove ic-connect packages

--- frontend/packages/ic-react-native/src/index.ts
+++
@@ -1,3 +0,0 @@
-export * from "./components";
-export * from "./identity-providers";
-export * from "./storage/storage";
--- frontend/packages/ic-react/src/hooks/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./useActor";
-export * from "./useAuth";
-export * from "./useClient";
-export * from "./useCurrentProvider";
-export * from "./useProviders";
